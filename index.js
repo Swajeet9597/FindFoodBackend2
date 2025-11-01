b452f6bd9f0912e16c33d43dad3d3f897e65dee5
Swajeet9597 <144508076+Swajeet9597@users.noreply.github.com>
2025-11-01 16:48:54 +0530

    Update index.js add quama after origin array

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require("cors")
 const cookieParser = require("cookie-parser")
 
 const corsOption = {
-    origin: ["http://localhost:4200", "http://localhost:3000"]
+    origin: ["http://localhost:4200", "http://localhost:3000"],
     methods:"POST,GET,PATCH,DELETE,HEAD",
     credentials: true,
 }
