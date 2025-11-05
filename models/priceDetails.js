22b278d6ac210c8f80ea83d853e6915a7e1ded26
Swajeet9597 <144508076+Swajeet9597@users.noreply.github.com>
2025-11-05 14:16:40 +0530

    Update priceDetails.js wth all fields requires
    Update priceDetails.js wth all fields requires

--- models/priceDetails.js
+++ models/priceDetails.js
@@ -1,25 +1,28 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const priceDetailsSchema = mongoose.Schema({
-    userId: {
-        type:String
-    },
+  userId: {
+    type: String,
+    required: true,
+  },
+  monthlyCharges: {
+    type: String,
+    required: true,
+  },
+  singleDayCharges: {
+    type: String,
+    required: true,
+  },
+  specialDayVegCharges: {
+    type: String,
+    required: true,
+  },
+  specialDaynonVegCharges: {
+    type: String,
+    required: true,
+  },
+});
 
-    monthlyCharges:{
-        type:String
-    },
-    singleDayCharges:{
-        type:String
-    },
-    specialDayVegCharges:{
-        type:String
-    },
-    specialDaynonVegCharges:{
-        type:String
-    }
-    
-})
-
-const PriceDetail = mongoose.model("PriceDetail", priceDetailsSchema)
+const PriceDetail = mongoose.model("PriceDetail", priceDetailsSchema);
 
 module.exports = PriceDetail;
