9b131b84332ed58d9f53cf45004c636d654e5c29
Swajeet9597 <144508076+Swajeet9597@users.noreply.github.com>
2025-11-05 14:32:24 +0530

    Update index.js Adding some corrs handleer
    Update index.js Adding some corrs handleer

--- index.js
+++ index.js
@@ -22,6 +22,22 @@ app.use(express.text());
 app.use("/api/user/",router)
 app.use("/api/user/",messDataRoutes)
 app.use("/api/user/",customerRoute)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+app.use((err, req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  console.error("Server Error:", err.message);
+  res.status(500).json({ message: "Internal Server Error" });
+});
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is listening on port ${process.env.PORT}`);
