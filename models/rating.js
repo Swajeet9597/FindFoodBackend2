c36f8d56754972c1f53a2362073fd4d3a43c8511
Swajeet9597 <144508076+Swajeet9597@users.noreply.github.com>
2025-11-05 14:17:41 +0530

    Update rating.js with validations
    Update rating.js with validations

--- models/rating.js
+++ models/rating.js
@@ -2,22 +2,22 @@ const mongoose = require("mongoose")
 
 const ratingSchema = mongoose.Schema({
     customerUserId: {
-        type:String
+        type: String,
+        required: true
     },
-
-    messUserId:{
-        type:String
+    messUserId: {
+        type: String,
+        required: true
     },
-    rating:{
-        type:Number
-    }
-    ,
-    feedback:{
-        type:String
+    rating: {
+        type: Number,
+        required: true
     },
-
-    
-},{timestamps: true})
+    feedback: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true })
 
 const Rating = mongoose.model("Rating", ratingSchema)
 
