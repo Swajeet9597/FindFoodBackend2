6dca69e44f31bff06b470adf799ff9669f5dbd0b
Swajeet9597 <144508076+Swajeet9597@users.noreply.github.com>
2025-11-06 13:24:53 +0530

    Update messDetails.js

--- models/messDetails.js
+++ models/messDetails.js
@@ -3,49 +3,62 @@ const mongoose = require("mongoose");
 const messDetailsSchema = mongoose.Schema({
   userId: {
     type: String,
+    required: true,
   },
   messName: {
     type: String,
+    required: true,
   },
   address: {
     shopNumber: {
       type: Number,
+      required: true,
     },
     area: {
       type: String,
+      required: true,
     },
     city: {
       type: String,
+      required: true,
     },
     pincode: {
       type: Number,
+      required: true,
     },
     landmark: {
       type: String,
+      required: true,
     },
   },
   contact: {
     mobileNumber: {
       type: Number,
+      required: true,
     },
     email: {
       type: String,
+      required: true,
     },
   },
   license: {
     licenseNumber: {
       type: String,
+      required: true,
     },
     licenseImage: {
       type: String,
+      required: true,
     },
   },
   foodType: {
     type: String,
+    required: true,
   },
   messImages: [
     {
       type: String,
+      required: true,
     },
   ],
 });
