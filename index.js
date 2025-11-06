899e04916e26847b08eaefab397d092be0a61bd6
Swajeet9597 <144508076+Swajeet9597@users.noreply.github.com>
2025-11-06 13:23:26 +0530

    Update index.js adding another origins
    Update index.js adding another origins

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require("cors")
 const cookieParser = require("cookie-parser")
 
 const corsOption = {
-    origin: "http://localhost:4200",
+    origin: ["http://localhost:4200","http://localhost:3000","http://localhost:5173"],
     methods:"POST,GET,PATCH,DELETE,HEAD",
     credentials: true,
 }
