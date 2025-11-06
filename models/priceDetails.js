3a585af33b869fdffc781d4754141a5df6374b60
Swajeet9597 <144508076+Swajeet9597@users.noreply.github.com>
2025-11-06 13:25:13 +0530

    Update priceDetails.js

--- models/priceDetails.js
+++ models/priceDetails.js
@@ -3,18 +3,23 @@ const mongoose = require("mongoose");
 const priceDetailsSchema = mongoose.Schema({
   userId: {
     type: String,
+    required: true,
   },
   monthlyCharges: {
     type: String,
+    required: true,
   },
   singleDayCharges: {
     type: String,
+    required: true,
   },
   specialDayVegCharges: {
     type: String,
+    required: true,
   },
   specialDaynonVegCharges: {
     type: String,
+    required: true,
   },
 });
 
